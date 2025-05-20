d536b1161b7c77abf4a18e7d41ada228fba0af4c
Justine23445 <justinecallysta@gmail.com>
2025-05-20 07:33:13 +0000

    Submit

---
+++ assignment_folder/assignment1a.py
@@ -0,0 +1,8 @@
+def greet(name) :
+    print(f"hello {name} ! Welcome to the game")
+
+player1 = input(f"What's the first player's name ? ")
+player2 = input(f"What's the second player's name ? ")
+
+greet(player1)
+greet(player2)
